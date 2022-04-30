99e33f45abcbae794c67f9bd1ee55d163057bc9c
Grant <gdblackx@me.com>
2022-04-30 14:27:53 -0400

    added load level and ui canvas

---
+++ Assets/Scripts/LSManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LSManager : MonoBehaviour
+{
+    public LSPlayer thePlayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void LoadLevel()
+    {
+        StartCoroutine(LoadLevelCoroutine());
+    }
+    IEnumerator LoadLevelCoroutine()
+    {
+        LSUIController.instance.FadeToBlack();
+        yield return new WaitForSeconds((1f / LSUIController.instance.fadeSpeed) * 0.25f);
+        SceneManager.LoadScene(thePlayer.currentPoint.levelToLoad);
+    }
+}
--- Assets/Scripts/LSPlayer.cs
+++ Assets/Scripts/LSPlayer.cs
@@ -6,7 +6,8 @@ public class LSPlayer : MonoBehaviour
 {
     public MapPoint currentPoint;
     public float moveSpeed = 10f;
-
+    private bool _levelLoading;
+    public LSManager lsManager;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +19,7 @@ public class LSPlayer : MonoBehaviour
     {
         transform.position = Vector3.MoveTowards(transform.position, currentPoint.transform.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, currentPoint.transform.position) < 0.1f)
+        if (Vector3.Distance(transform.position, currentPoint.transform.position) < 0.1f && !_levelLoading)
         {
 
 
@@ -48,6 +49,15 @@ public class LSPlayer : MonoBehaviour
                 if (currentPoint.down != null)
                 {
                     SetNextPoint(currentPoint.down);
+                }
+            }
+            if (currentPoint.isLevel)
+            {
+                if (Input.GetButtonDown("Jump"))
+                {
+                    // load the level
+                    _levelLoading = true;
+                    lsManager.LoadLevel();
                 }
             }
         }
