d2bd20e69645c2dd29ea12bada7d3fb8dd8f761b
Grant <gdblackx@me.com>
2022-06-15 22:40:12 -0400

    added attack logic for flying enemy

--- Assets/Scripts/FlyingEnemyController.cs
+++ Assets/Scripts/FlyingEnemyController.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class FlyingEnemyController : MonoBehaviour
 {
     public Transform[] points;
-    public float moveSpeed;
+    public float moveSpeed, chaseSpeed, distanceToAttackPlayer, waitAfterAttack;
     private int currentPoint;
     public SpriteRenderer spriteRenderer;
+
+    private Vector3 attackTarget;
+    private float attackCounter;
 
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < points.Length; i++)
+        for (int i = 0; i < points.Length; i++)
         {
             points[i].parent = null;
         }
@@ -21,21 +24,50 @@ public class FlyingEnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, points[currentPoint].position, moveSpeed * Time.deltaTime);
-        if (Vector3.Distance(transform.position, points[currentPoint].position) < 0.05f)
+        if (attackCounter > 0)
         {
-            currentPoint++;
+            attackCounter -= Time.deltaTime;
         }
-        if (currentPoint >= points.Length)
+        else
         {
-            currentPoint = 0;
-        }
-        if (transform.position.x < points[currentPoint].position.x)
-        {
-            spriteRenderer.flipX = true;
-        } else if (transform.position.x > points[currentPoint].position.x)
-        {
-            spriteRenderer.flipX = false;
+            if (Vector3.Distance(transform.position, PlayerController.instance.transform.position) > distanceToAttackPlayer)
+            {
+                attackTarget = Vector3.zero;
+
+                transform.position = Vector3.MoveTowards(transform.position, points[currentPoint].position, moveSpeed * Time.deltaTime);
+                if (Vector3.Distance(transform.position, points[currentPoint].position) < 0.05f)
+                {
+                    currentPoint++;
+                }
+                if (currentPoint >= points.Length)
+                {
+                    currentPoint = 0;
+                }
+                if (transform.position.x < points[currentPoint].position.x)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                else if (transform.position.x > points[currentPoint].position.x)
+                {
+                    spriteRenderer.flipX = false;
+                }
+            }
+            else
+            {
+                // Attacking the Player
+                if (attackTarget == Vector3.zero)
+                {
+                    attackTarget = PlayerController.instance.transform.position;
+                }
+                transform.position = Vector3.MoveTowards(transform.position,
+                    attackTarget, chaseSpeed * Time.deltaTime);
+
+                if (Vector3.Distance(transform.position, attackTarget) <= 0.1f)
+                {
+                    attackCounter = waitAfterAttack;
+                    attackTarget = Vector3.zero;
+                }
+            }
         }
     }
 }
